7988351be400bc6f26353f30db6fd562d162f248
danielxiquin <danielxiquin802@gmail.com>
2025-04-27 01:05:46 -0600

    se ejecuto el comando java -jar ../lib/antlr-4.13.2-complete.jar -visitor MyLanguage.g4   para obtener los archivos de la gramatica y el comando de ejecucion de los archivo java para obtener los .class

---
+++ grammar/MyLanguageBaseListener.java
@@ -0,0 +1,411 @@
+// Generated from MyLanguage.g4 by ANTLR 4.13.2
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+/**
+ * This class provides an empty implementation of {@link MyLanguageListener},
+ * which can be extended to create a listener which only needs to handle a subset
+ * of the available methods.
+ */
+@SuppressWarnings("CheckReturnValue")
+public class MyLanguageBaseListener implements MyLanguageListener {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterProgram(MyLanguageParser.ProgramContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitProgram(MyLanguageParser.ProgramContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterStatementList(MyLanguageParser.StatementListContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitStatementList(MyLanguageParser.StatementListContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterMain(MyLanguageParser.MainContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitMain(MyLanguageParser.MainContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterStatementListTail(MyLanguageParser.StatementListTailContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitStatementListTail(MyLanguageParser.StatementListTailContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterStatement(MyLanguageParser.StatementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitStatement(MyLanguageParser.StatementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterVariableDeclarationEmpty(MyLanguageParser.VariableDeclarationEmptyContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitVariableDeclarationEmpty(MyLanguageParser.VariableDeclarationEmptyContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterVariableAssigment(MyLanguageParser.VariableAssigmentContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitVariableAssigment(MyLanguageParser.VariableAssigmentContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterVariableDeclarationValue(MyLanguageParser.VariableDeclarationValueContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitVariableDeclarationValue(MyLanguageParser.VariableDeclarationValueContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterFunctionCallExpr(MyLanguageParser.FunctionCallExprContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitFunctionCallExpr(MyLanguageParser.FunctionCallExprContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterArgumentList(MyLanguageParser.ArgumentListContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitArgumentList(MyLanguageParser.ArgumentListContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterFunctionVariableDeclaration(MyLanguageParser.FunctionVariableDeclarationContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitFunctionVariableDeclaration(MyLanguageParser.FunctionVariableDeclarationContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterVariable(MyLanguageParser.VariableContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitVariable(MyLanguageParser.VariableContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterFigure(MyLanguageParser.FigureContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitFigure(MyLanguageParser.FigureContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterAritmetica(MyLanguageParser.AritmeticaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitAritmetica(MyLanguageParser.AritmeticaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterWhichCondition(MyLanguageParser.WhichConditionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitWhichCondition(MyLanguageParser.WhichConditionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterCondition(MyLanguageParser.ConditionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitCondition(MyLanguageParser.ConditionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterNumbersCondition(MyLanguageParser.NumbersConditionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitNumbersCondition(MyLanguageParser.NumbersConditionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterExpression1(MyLanguageParser.Expression1Context ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitExpression1(MyLanguageParser.Expression1Context ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterExpression2(MyLanguageParser.Expression2Context ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitExpression2(MyLanguageParser.Expression2Context ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterExpression3(MyLanguageParser.Expression3Context ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitExpression3(MyLanguageParser.Expression3Context ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterRelationOperator2(MyLanguageParser.RelationOperator2Context ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitRelationOperator2(MyLanguageParser.RelationOperator2Context ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterIfStatement(MyLanguageParser.IfStatementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitIfStatement(MyLanguageParser.IfStatementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterElsePart(MyLanguageParser.ElsePartContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitElsePart(MyLanguageParser.ElsePartContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterWhileStatement(MyLanguageParser.WhileStatementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitWhileStatement(MyLanguageParser.WhileStatementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterFunctionCall(MyLanguageParser.FunctionCallContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitFunctionCall(MyLanguageParser.FunctionCallContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterFunctionDeclaration(MyLanguageParser.FunctionDeclarationContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitFunctionDeclaration(MyLanguageParser.FunctionDeclarationContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterParameterList(MyLanguageParser.ParameterListContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitParameterList(MyLanguageParser.ParameterListContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterParameterListTail(MyLanguageParser.ParameterListTailContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitParameterListTail(MyLanguageParser.ParameterListTailContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterInputStatement(MyLanguageParser.InputStatementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitInputStatement(MyLanguageParser.InputStatementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterOutputStatement(MyLanguageParser.OutputStatementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitOutputStatement(MyLanguageParser.OutputStatementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterOperator(MyLanguageParser.OperatorContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitOperator(MyLanguageParser.OperatorContext ctx) { }
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitTerminal(TerminalNode node) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitErrorNode(ErrorNode node) { }
+}
---
+++ grammar/MyLanguageListener.java
@@ -0,0 +1,319 @@
+// Generated from MyLanguage.g4 by ANTLR 4.13.2
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link MyLanguageParser}.
+ */
+public interface MyLanguageListener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link MyLanguageParser#program}.
+	 * @param ctx the parse tree
+	 */
+	void enterProgram(MyLanguageParser.ProgramContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link MyLanguageParser#program}.
+	 * @param ctx the parse tree
+	 */
+	void exitProgram(MyLanguageParser.ProgramContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link MyLanguageParser#statementList}.
+	 * @param ctx the parse tree
+	 */
+	void enterStatementList(MyLanguageParser.StatementListContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link MyLanguageParser#statementList}.
+	 * @param ctx the parse tree
+	 */
+	void exitStatementList(MyLanguageParser.StatementListContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link MyLanguageParser#main}.
+	 * @param ctx the parse tree
+	 */
+	void enterMain(MyLanguageParser.MainContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link MyLanguageParser#main}.
+	 * @param ctx the parse tree
+	 */
+	void exitMain(MyLanguageParser.MainContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link MyLanguageParser#statementListTail}.
+	 * @param ctx the parse tree
+	 */
+	void enterStatementListTail(MyLanguageParser.StatementListTailContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link MyLanguageParser#statementListTail}.
+	 * @param ctx the parse tree
+	 */
+	void exitStatementListTail(MyLanguageParser.StatementListTailContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link MyLanguageParser#statement}.
+	 * @param ctx the parse tree
+	 */
+	void enterStatement(MyLanguageParser.StatementContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link MyLanguageParser#statement}.
+	 * @param ctx the parse tree
+	 */
+	void exitStatement(MyLanguageParser.StatementContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link MyLanguageParser#variableDeclarationEmpty}.
+	 * @param ctx the parse tree
+	 */
+	void enterVariableDeclarationEmpty(MyLanguageParser.VariableDeclarationEmptyContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link MyLanguageParser#variableDeclarationEmpty}.
+	 * @param ctx the parse tree
+	 */
+	void exitVariableDeclarationEmpty(MyLanguageParser.VariableDeclarationEmptyContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link MyLanguageParser#variableAssigment}.
+	 * @param ctx the parse tree
+	 */
+	void enterVariableAssigment(MyLanguageParser.VariableAssigmentContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link MyLanguageParser#variableAssigment}.
+	 * @param ctx the parse tree
+	 */
+	void exitVariableAssigment(MyLanguageParser.VariableAssigmentContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link MyLanguageParser#variableDeclarationValue}.
+	 * @param ctx the parse tree
+	 */
+	void enterVariableDeclarationValue(MyLanguageParser.VariableDeclarationValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link MyLanguageParser#variableDeclarationValue}.
+	 * @param ctx the parse tree
+	 */
+	void exitVariableDeclarationValue(MyLanguageParser.VariableDeclarationValueContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link MyLanguageParser#functionCallExpr}.
+	 * @param ctx the parse tree
+	 */
+	void enterFunctionCallExpr(MyLanguageParser.FunctionCallExprContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link MyLanguageParser#functionCallExpr}.
+	 * @param ctx the parse tree
+	 */
+	void exitFunctionCallExpr(MyLanguageParser.FunctionCallExprContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link MyLanguageParser#argumentList}.
+	 * @param ctx the parse tree
+	 */
+	void enterArgumentList(MyLanguageParser.ArgumentListContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link MyLanguageParser#argumentList}.
+	 * @param ctx the parse tree
+	 */
+	void exitArgumentList(MyLanguageParser.ArgumentListContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link MyLanguageParser#functionVariableDeclaration}.
+	 * @param ctx the parse tree
+	 */
+	void enterFunctionVariableDeclaration(MyLanguageParser.FunctionVariableDeclarationContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link MyLanguageParser#functionVariableDeclaration}.
+	 * @param ctx the parse tree
+	 */
+	void exitFunctionVariableDeclaration(MyLanguageParser.FunctionVariableDeclarationContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link MyLanguageParser#variable}.
+	 * @param ctx the parse tree
+	 */
+	void enterVariable(MyLanguageParser.VariableContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link MyLanguageParser#variable}.
+	 * @param ctx the parse tree
+	 */
+	void exitVariable(MyLanguageParser.VariableContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link MyLanguageParser#figure}.
+	 * @param ctx the parse tree
+	 */
+	void enterFigure(MyLanguageParser.FigureContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link MyLanguageParser#figure}.
+	 * @param ctx the parse tree
+	 */
+	void exitFigure(MyLanguageParser.FigureContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link MyLanguageParser#aritmetica}.
+	 * @param ctx the parse tree
+	 */
+	void enterAritmetica(MyLanguageParser.AritmeticaContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link MyLanguageParser#aritmetica}.
+	 * @param ctx the parse tree
+	 */
+	void exitAritmetica(MyLanguageParser.AritmeticaContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link MyLanguageParser#whichCondition}.
+	 * @param ctx the parse tree
+	 */
+	void enterWhichCondition(MyLanguageParser.WhichConditionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link MyLanguageParser#whichCondition}.
+	 * @param ctx the parse tree
+	 */
+	void exitWhichCondition(MyLanguageParser.WhichConditionContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link MyLanguageParser#condition}.
+	 * @param ctx the parse tree
+	 */
+	void enterCondition(MyLanguageParser.ConditionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link MyLanguageParser#condition}.
+	 * @param ctx the parse tree
+	 */
+	void exitCondition(MyLanguageParser.ConditionContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link MyLanguageParser#numbersCondition}.
+	 * @param ctx the parse tree
+	 */
+	void enterNumbersCondition(MyLanguageParser.NumbersConditionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link MyLanguageParser#numbersCondition}.
+	 * @param ctx the parse tree
+	 */
+	void exitNumbersCondition(MyLanguageParser.NumbersConditionContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link MyLanguageParser#expression1}.
+	 * @param ctx the parse tree
+	 */
+	void enterExpression1(MyLanguageParser.Expression1Context ctx);
+	/**
+	 * Exit a parse tree produced by {@link MyLanguageParser#expression1}.
+	 * @param ctx the parse tree
+	 */
+	void exitExpression1(MyLanguageParser.Expression1Context ctx);
+	/**
+	 * Enter a parse tree produced by {@link MyLanguageParser#expression2}.
+	 * @param ctx the parse tree
+	 */
+	void enterExpression2(MyLanguageParser.Expression2Context ctx);
+	/**
+	 * Exit a parse tree produced by {@link MyLanguageParser#expression2}.
+	 * @param ctx the parse tree
+	 */
+	void exitExpression2(MyLanguageParser.Expression2Context ctx);
+	/**
+	 * Enter a parse tree produced by {@link MyLanguageParser#expression3}.
+	 * @param ctx the parse tree
+	 */
+	void enterExpression3(MyLanguageParser.Expression3Context ctx);
+	/**
+	 * Exit a parse tree produced by {@link MyLanguageParser#expression3}.
+	 * @param ctx the parse tree
+	 */
+	void exitExpression3(MyLanguageParser.Expression3Context ctx);
+	/**
+	 * Enter a parse tree produced by {@link MyLanguageParser#relationOperator2}.
+	 * @param ctx the parse tree
+	 */
+	void enterRelationOperator2(MyLanguageParser.RelationOperator2Context ctx);
+	/**
+	 * Exit a parse tree produced by {@link MyLanguageParser#relationOperator2}.
+	 * @param ctx the parse tree
+	 */
+	void exitRelationOperator2(MyLanguageParser.RelationOperator2Context ctx);
+	/**
+	 * Enter a parse tree produced by {@link MyLanguageParser#ifStatement}.
+	 * @param ctx the parse tree
+	 */
+	void enterIfStatement(MyLanguageParser.IfStatementContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link MyLanguageParser#ifStatement}.
+	 * @param ctx the parse tree
+	 */
+	void exitIfStatement(MyLanguageParser.IfStatementContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link MyLanguageParser#elsePart}.
+	 * @param ctx the parse tree
+	 */
+	void enterElsePart(MyLanguageParser.ElsePartContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link MyLanguageParser#elsePart}.
+	 * @param ctx the parse tree
+	 */
+	void exitElsePart(MyLanguageParser.ElsePartContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link MyLanguageParser#whileStatement}.
+	 * @param ctx the parse tree
+	 */
+	void enterWhileStatement(MyLanguageParser.WhileStatementContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link MyLanguageParser#whileStatement}.
+	 * @param ctx the parse tree
+	 */
+	void exitWhileStatement(MyLanguageParser.WhileStatementContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link MyLanguageParser#functionCall}.
+	 * @param ctx the parse tree
+	 */
+	void enterFunctionCall(MyLanguageParser.FunctionCallContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link MyLanguageParser#functionCall}.
+	 * @param ctx the parse tree
+	 */
+	void exitFunctionCall(MyLanguageParser.FunctionCallContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link MyLanguageParser#functionDeclaration}.
+	 * @param ctx the parse tree
+	 */
+	void enterFunctionDeclaration(MyLanguageParser.FunctionDeclarationContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link MyLanguageParser#functionDeclaration}.
+	 * @param ctx the parse tree
+	 */
+	void exitFunctionDeclaration(MyLanguageParser.FunctionDeclarationContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link MyLanguageParser#parameterList}.
+	 * @param ctx the parse tree
+	 */
+	void enterParameterList(MyLanguageParser.ParameterListContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link MyLanguageParser#parameterList}.
+	 * @param ctx the parse tree
+	 */
+	void exitParameterList(MyLanguageParser.ParameterListContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link MyLanguageParser#parameterListTail}.
+	 * @param ctx the parse tree
+	 */
+	void enterParameterListTail(MyLanguageParser.ParameterListTailContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link MyLanguageParser#parameterListTail}.
+	 * @param ctx the parse tree
+	 */
+	void exitParameterListTail(MyLanguageParser.ParameterListTailContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link MyLanguageParser#inputStatement}.
+	 * @param ctx the parse tree
+	 */
+	void enterInputStatement(MyLanguageParser.InputStatementContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link MyLanguageParser#inputStatement}.
+	 * @param ctx the parse tree
+	 */
+	void exitInputStatement(MyLanguageParser.InputStatementContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link MyLanguageParser#outputStatement}.
+	 * @param ctx the parse tree
+	 */
+	void enterOutputStatement(MyLanguageParser.OutputStatementContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link MyLanguageParser#outputStatement}.
+	 * @param ctx the parse tree
+	 */
+	void exitOutputStatement(MyLanguageParser.OutputStatementContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link MyLanguageParser#operator}.
+	 * @param ctx the parse tree
+	 */
+	void enterOperator(MyLanguageParser.OperatorContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link MyLanguageParser#operator}.
+	 * @param ctx the parse tree
+	 */
+	void exitOperator(MyLanguageParser.OperatorContext ctx);
+}
