a2f6d99fcdfbac56e235d06d78f581a472c02a58
hayyrs66 <rayrtsx@proton.me>
2025-04-27 20:10:18 -0600

    Implementación de métodos visit para MainNode y StatementListTailNode en ASTBuilder, y ampliación de la lógica en visitStatement para manejar diferentes tipos de instrucciones.

--- src/ASTBuilder.java
+++ src/ASTBuilder.java
@@ -23,4 +23,45 @@ public class ASTBuilder extends MyLanguageBaseVisitor<ASTNode> {
             return new StatementListNode(ctx.getStart().getLine(), ctx.getStart().getCharPositionInLine(), functionDeclaration, nextStatementList);
         }
     }
+
+    @Override
+    public ASTNode visitMain(MyLanguageParser.MainContext ctx) {
+        StatementNode statement = (StatementNode) visit(ctx.statement());
+        StatementListTailNode statementListTail = (StatementListTailNode) visit(ctx.statementListTail());
+        return new MainNode(ctx.getStart().getLine(), ctx.getStart().getCharPositionInLine(), statement, statementListTail);
+    }
+
+    @Override
+    public ASTNode visitStatementListTail(MyLanguageParser.StatementListTailContext ctx) {
+        if (ctx.statement() != null) {
+            StatementNode statement = (StatementNode) visit(ctx.statement());
+            StatementListTailNode next = (StatementListTailNode) visit(ctx.statementListTail());
+            return new StatementListTailNode(ctx.getStart().getLine(), ctx.getStart().getCharPositionInLine(), statement, next);
+        } else {
+            // Caso epsilon
+            return new StatementListTailNode(ctx.getStart().getLine(), ctx.getStart().getCharPositionInLine());
+        }
+    }
+
+    @Override
+    public ASTNode visitStatement(MyLanguageParser.StatementContext ctx) {
+        if (ctx.variableDeclarationEmpty() != null) {
+            return visit(ctx.variableDeclarationEmpty());
+        } else if (ctx.ifStatement() != null) {
+            return visit(ctx.ifStatement());
+        } else if (ctx.whileStatement() != null) {
+            return visit(ctx.whileStatement());
+        } else if (ctx.functionCall() != null) {
+            return visit(ctx.functionCall());
+        } else if (ctx.inputStatement() != null) {
+            return visit(ctx.inputStatement());
+        } else if (ctx.outputStatement() != null) {
+            return visit(ctx.outputStatement());
+        } else if (ctx.variableAssigment() != null) {
+            return visit(ctx.variableAssigment());
+        }
+        throw new RuntimeException("Tipo de instrucción no reconocida");
+    }
+
+   
 }
