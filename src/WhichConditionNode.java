f05d993ce17af93c02beaacd6041d0808552c02a
hayyrs66 <rayrtsx@proton.me>
2025-04-27 20:13:58 -0600

    Implementación de la clase WhichConditionNode para manejar condiciones booleanas y nodos de condición en el AST

---
+++ src/WhichConditionNode.java
@@ -0,0 +1,22 @@
+public class WhichConditionNode extends ASTNode {
+    private Boolean boolValue;
+    private ConditionNode condition;
+
+    // Constructor para boolean
+    public WhichConditionNode(int line, int column, Boolean boolValue) {
+        super(line, column);
+        this.boolValue = boolValue;
+        this.condition = null;
+    }
+
+    // Constructor para condición
+    public WhichConditionNode(int line, int column, ConditionNode condition) {
+        super(line, column);
+        this.boolValue = null;
+        this.condition = condition;
+    }
+
+    public Boolean getBoolValue() {
+        return boolValue;
+    }
+}
